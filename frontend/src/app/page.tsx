c592262724bfd70cffda5a3296f7c0787c652284
Ethan-Huang21 <ethan.huang@ucalgary.ca>
2024-03-02 00:09:45 -0700

    Added Sidebar and modified structure of containers

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -73,38 +73,49 @@ export default function Home() {
   }, [messages])
 
   return (
-    <div>
-      <div className="overflow-scroll" style={{ height: "85vh" }}>
-        {messages?.map(message =>
-          <div
-            className={message.username == user?.username ? "flex justify-end mx-10 my-5" : "flex mx-10 my-5"}
-            key={message.id}>
-            <div className="p-3 border-solid border-2 border-indigo-600 rounded-lg bg-indigo-600 text-white">
-              <div className="flex items-center">
-                <div className="font-bold text-lg mr-2">{message.username}
-                </div>
-                <div className="text-sm text-grey">{message.time.slice(10, 16)}</div>
-              </div>
-              <div>{message.content}</div>
-            </div>
-          </div>)}
-        <div ref={messagesEndRef}></div>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="bg-gray-800 text-white p-8">
+        <p>A</p>
       </div>
-      <div className="flex m-10 absolute inset-x-0 bottom-0">
-        <input
-          onChange={e => setInput(e.currentTarget.value)}
-          onKeyDown={e => {
-            e.key == "Enter" && sendMessage();
-          }}
-          value={input}
-          type="text"
-          className="w-full rounded-lg border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-        <button className="ml-5" onClick={sendMessage}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-          </svg>
-        </button>
+
+      {/* Primary Flex Container for Input + Response */}
+      <div className="flex flex-col w-full bg-gray-100">
+        {/* Overflow Container for Message Fetching */}
+        <div className="flex-grow overflow-scroll bg-blue-100" style={{ height: "85vh", overflowX: "auto" }}>
+          {messages?.map(message =>
+            <div
+              className={message.username == user?.username ? "flex justify-end mx-10 my-5" : "flex mx-10 my-5"}
+              key={message.id}>
+              <div className="p-3 border-solid border-2 border-indigo-600 rounded-lg bg-indigo-600 text-white">
+                <div className="flex items-center">
+                  <div className="font-bold text-lg mr-2">{message.username}
+                  </div>
+                  <div className="text-sm text-grey">{message.time.slice(10, 16)}</div>
+                </div>
+                <div>{message.content}</div>
+              </div>
+            </div>)}
+          <div ref={messagesEndRef}></div>
+        </div>
+
+        {/* Flex Container for Message Input */}
+        <div className="flex m-2 p-4 mt-auto bg-red-100">
+          <input
+            onChange={e => setInput(e.currentTarget.value)}
+            onKeyDown={e => {
+              e.key == "Enter" && sendMessage();
+            }}
+            value={input}
+            type="text"
+            className="w-full rounded-lg border-0 py-1.5 pl-5 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+          <button className="ml-5" onClick={sendMessage}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
